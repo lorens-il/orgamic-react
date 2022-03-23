ce862ebb54cee41aafb0ac41b9ea124ea4ff8cfe
Ilya <lawrence.il.pr@gmail.com>
2022-03-23 23:21:07 +0500

    menu functional  has been created

---
+++ src/components/discounts/Discounts.tsx
@@ -0,0 +1,16 @@
+import { FC } from "react";
+
+const Discounts: FC = () => {
+    return (
+        <div className="discounts">
+            <div className="container">
+                <div className="discounts__wrapper">
+                    .
+                </div>  
+            </div>    
+        </div>
+
+    )
+}
+
+export default Discounts;
