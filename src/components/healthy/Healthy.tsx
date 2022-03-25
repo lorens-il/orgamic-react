afd8f41a80affafa3df36faa44b698390eca71a5
Ilya <lawrence.il.pr@gmail.com>
2022-03-26 01:21:03 +0500

    'healthy' has been begun creating

---
+++ src/components/healthy/Healthy.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+import "./healthy.sass";
+
+const Healthy: FC = () => {
+    return (
+        <div className="healthy">
+
+        </div>
+    )
+}
+
+export default Healthy;
--- src/components/pages/productPage/ProductPage.tsx
+++ src/components/pages/productPage/ProductPage.tsx
@@ -1,6 +1,7 @@
 import {FC} from "react";
 import Discounts from "../../discounts/Discounts";
 import Header from "../../header/Header";
+import Healthy from "../../healthy/Healthy";
 import Promo from "../../promo/Promo";
 
 const ProductPage: FC = () => {
@@ -9,6 +10,7 @@ const ProductPage: FC = () => {
             <Header/>
             <Promo/>
             <Discounts/>
+            <Healthy/>
         </>
     )
 }
