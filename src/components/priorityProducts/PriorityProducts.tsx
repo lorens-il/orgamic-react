8c720d9bc8857f2fed226d31ae0cb7b8f36c0630
Ilya <lawrence.il.pr@gmail.com>
2022-04-01 23:43:16 +0500

    Carousels functionality  have been created in 'priority-products'. Duplicate key fixed

--- src/components/priorityProducts/PriorityProducts.tsx
+++ src/components/priorityProducts/PriorityProducts.tsx
@@ -1,10 +1,35 @@
-import { FC } from "react"; 
+import { FC, useRef } from "react"; 
 import { useGetProductsQuery } from "../../api/apiSlice";
 import arrow from "./arrow.svg"
 
 import "./priorityProducts.sass";
 
 const PriorityProducts: FC = () => {
+    
+    const refBorder = useRef<any[]>([]);
+    const refTitle = useRef<any[]>([]);
+    const refBtns = useRef<any[]>([]);
+
+    const onShift = (e: any) => {
+        e.preventDefault();
+        const grayBorder = "1px solid rgb(221, 221, 221)",
+              greenBorder = "1px solid rgb(118, 167, 19)";
+        
+        refBorder.current.forEach((item, i) => {
+            console.log(e.currentTarget.dataset.index, i, item.style.border);
+            if (+e.currentTarget.dataset.index === i && item.style.border !== grayBorder) {
+                item.style.border = grayBorder; 
+                item.style.background = "rgb(255, 255, 255)";
+                refTitle.current[i].style.color = "rgb(118, 167, 19)";
+                refBtns.current[i].style.left = "54.4%"
+            } else if (+e.currentTarget.dataset.index === i && item.style.border !== greenBorder) {
+                item.style.cssText = ''
+                refTitle.current[i].style.cssText = ''
+                refBtns.current[i].style.cssText = ''
+            }
+            
+        });
+    }
 
     const {
         data = [],
@@ -13,25 +38,25 @@ const PriorityProducts: FC = () => {
     } = useGetProductsQuery();
 
     const creatingContent = () => {
-        return data.map(({id, url, name, desc, categories}) => {
+        return data.map(({id, url, name, desc, categories}, i) => {
             if (categories === false) return null;
             return (
-                <div key={id} className="priority-products__item">
+                <div key={id} ref={item => refBorder.current.push(item)} className="priority-products__item">
                     <div className="priority-products__wrapper-img">
                         <img src={url} alt={name} />    
                     </div>
-                    <h2 className="priority-products__title">{name}</h2>
+                    <h2 ref={item => refTitle.current.push(item)} className="priority-products__title">{name}</h2>
                     <div className="priority-products__text">
                         {desc}
                     </div>
-                    <div className="priority-products__btns">
+                    <div ref={item => refBtns.current.push(item)} className="priority-products__btns">
                         <div className="priority-products__wrapper">
-                            <a href="/" className="link link_small">
+                            <a className="link link_small" href="/" data-index={i} onClick={onShift}>
                                 <div>shop now</div>
                             </a>    
                         </div>
                         <div className="priority-products__wrapper">
-                            <div className="priority-products__arrow">
+                            <div className="priority-products__arrow" data-index={i} onClick={onShift}>
                                 <img src={arrow} alt="arrow" />
                             </div>      
                         </div>
