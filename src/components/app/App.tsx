fcc11a90e37b01e60d747aae20b44f056730299e
Ilya <lawrence.il.pr@gmail.com>
2022-05-09 01:33:55 +0500

    change

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,10 +1,11 @@
-import {FC, lazy} from 'react';
+import {FC, lazy, Suspense} from 'react';
 import {BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
 import AboutUsPage from '../pages/aboutPage/AboutPage';
 import Page404 from '../pages/page404/Page404';
+import Spinner from '../spinner/Spinner';
 
 import "../../styles/link.sass";
 
@@ -15,12 +16,14 @@ const App: FC = () => {
     return (
         <BrowserRouter>
             <Header/>
-            <Routes>
-                <Route path='/' element={<ProductPage />}/>
-                <Route path='about' element={<AboutUsPage/>}/>
-                <Route path='cart' element={<Cart/>}/>
-                <Route path='*' element={<Page404/>}/>
-            </Routes>
+            <Suspense fallback={<Spinner/>}>
+                <Routes>
+                    <Route path='/' element={<ProductPage />}/>
+                    <Route path='about' element={<AboutUsPage/>}/>
+                    <Route path='cart' element={<Cart/>}/>
+                    <Route path='*' element={<Page404/>}/>
+                </Routes>
+            </Suspense>
             <Footer/>
         </BrowserRouter>
     );
